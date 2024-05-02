e4264e4f30b82b9bb8abebeb9c493c86fca8ae48
AhmedNasser1010 <ahmed.mix.games@gmail.com>
2024-05-03 00:11:22 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # emirates-adventure
 # portfolio
+# portfolio
---
+++ script.js
@@ -0,0 +1,79 @@
+// ==Start splide slider
+// initialize
+const splide = new Splide('.splide', {
+	type: 'loop',
+	autoplay: false,
+  interval: 2000,
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	splide.mount();
+});
+
+// Handle custom arrows
+document.querySelector('.custom__arrow--prev').addEventListener('click', function () {
+	splide.go('-');
+});
+
+document.querySelector('.custom__arrow--next').addEventListener('click', function () {
+	splide.go('+');
+});
+// ==End splide slider
+
+// ==Start nav handler
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = () => {
+
+  let currentScrollPos = window.pageYOffset;
+  const nav = document.querySelector(".navbar");
+  const scrollUpBtn = document.querySelector(".back-to-btn");
+
+  if (window.innerWidth > 480) {
+
+    if (currentScrollPos < 250) {
+      scrollUpBtn.style.opacity = '0';
+      scrollUpBtn.style.visibility = 'hidden';
+    } else {
+      scrollUpBtn.style.opacity = '1';
+      scrollUpBtn.style.visibility = 'visible';
+    }
+
+    if (prevScrollpos > currentScrollPos) {
+      nav.style.opacity = "1";
+      nav.style.visibility = 'visible';
+    } else {
+      nav.style.opacity = "0";
+      nav.style.visibility = 'hidden';
+    }
+    prevScrollpos = currentScrollPos;
+
+  }
+
+}
+// ==Start nav handler
+
+// ==Start handle nav visibility in mobile
+const menuIcon = document.querySelector('.menu-icon');
+const nav = document.querySelector(".navbar");
+
+menuIcon.addEventListener('mouseup', () => {
+
+  nav.classList.contains('active') ? nav.classList.remove('active') : nav.classList.add('active');
+  menuIcon.classList.contains('active') ? menuIcon.classList.remove('active') : menuIcon.classList.add('active');
+  menuIcon.classList.contains('active') ? menuIcon.classList.replace('fa-bars', 'fa-minus') : menuIcon.classList.replace('fa-minus', 'fa-bars');
+
+})
+
+const childsArr = Array.from(nav.children);
+
+childsArr.map(child => {
+  !child.classList.contains('search-box') && 
+    child.addEventListener('mouseup', () => {
+      menuIcon.classList.replace('fa-minus', 'fa-bars');
+      menuIcon.classList.remove('active');
+      nav.classList.remove('active');
+    })
+})
+
+// ==End handle nav visibility in mobile
